30904214669fb2efd09d57d9b3a70fb24d776051
Danila <blikoff.d@ya.ru>
2021-04-05 18:03:53 +0300

    feature 5

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import "./styles.css"
 import data from "./data.js"
 
-const container = document.querySelector(".container")
-
-const createDiv = (dataItem, dataAttrValue, id) => {
+const createDiv = (dataItem, dataAttrValue) => {
   const div = document.createElement("div")
   div.insertAdjacentHTML("beforeend", `${dataItem.id} ${dataItem.feature}`)
   div.setAttribute(dataAttrValue, "")
-  div.setAttribute("id", id)
+  div.setAttribute("id", dataItem.id)
   return div
 }
 
-const result = data.reduce((acc, currentDataElement) => {
+const sortedData = data.reduce((acc, currentDataElement) => {
   const [others, penultimates, lasts] = acc
-  acc.map(itemsList => itemsList.sort((a,b) => b["id"] - a["id"]))
 
   switch (currentDataElement.feature) {
     case "last":
-      lasts.push(createDiv(currentDataElement, "data-featured-last",currentDataElement.id))
-      break
-    case "other":
-      others.push(createDiv(currentDataElement, "data-featured-other",currentDataElement.id))
+      lasts.push(createDiv(currentDataElement, "data-featured-last"))
       break
     case "penultimate":
-      penultimates.push(createDiv(currentDataElement, "data-featured-penultimate",currentDataElement.id))
+      penultimates.push(createDiv(currentDataElement, "data-featured-penultimate"))
+      break
+    default:
+      others.push(createDiv(currentDataElement, "data-featured-other"))
       break
   }
   
   return acc
-}, [[],[],[]]).flat()
+}, [[],[],[]])
+    .map(el => el.sort((a,b) => b["id"] - a["id"]))
+    .flat()
 
-container.replaceChildren(...result)
+document.querySelector(".container").replaceChildren(...sortedData)
