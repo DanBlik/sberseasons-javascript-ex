bc4efbb3daca1ca5af0fcab58c6a53a4d509f0ff
danblik <blikoff.d@ya.ru>
2021-04-06 18:34:22 +0300

    feature 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import ItemsList from "./components/ItemsList";
+import ItemsList from "./components/ItemsList"
+import data from './data'
 
 function App() {
+  const sortedData = data.reduce((acc, currentDataElement) => {
+    const [others, penultimates, lasts] = acc
+    const {id, feature} = currentDataElement
+
+    switch (currentDataElement.feature) {
+      case "last":
+        lasts.push({id, feature})
+        break
+      case "penultimate":
+        penultimates.push({id, feature})
+        break
+      default:
+        others.push({id, feature})
+    }
+    
+    return acc
+  }, [[],[],[]])
+      .map(el => el.sort((a,b) => b["id"] - a["id"])).flat()
+
   return (
     <div className="App">
-      <ItemsList/>
+      <ItemsList sortedData={sortedData}/>
     </div>
   );
 }
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,31 +1,14 @@
 import React from 'react'
-import Card from './Card'
-import data from '../data'
 
-const ItemsList = () => {
-    const sortedData = data.reduce((acc, currentDataElement) => {
-        const [others, penultimates, lasts] = acc
-        const {id, feature} = currentDataElement
-
-        switch (currentDataElement.feature) {
-          case "last":
-            lasts.push({id, feature, dataAttribute: `data-featured-${feature}`})
-            break
-          case "penultimate":
-            penultimates.push({id, feature, dataAttribute: `data-featured-${feature}`})
-            break
-          default:
-            others.push({id, feature, dataAttribute: `data-featured-${feature}`})
-        }
-        
-        return acc
-      }, [[],[],[]])
-          .map(el => el.sort((a,b) => b["id"] - a["id"])).flat()
-
+const ItemsList = ({sortedData}) => {
     return (
         <div data-container>
             {
-                sortedData.map(({id, feature, dataAttribute}) => <Card key={id} id={id} feature={feature} dataAttribute={{[dataAttribute]: ""}}/>)
+                sortedData.map(({id, feature}) => {
+                    return (
+                        <div key={id} id={id} feature={feature} {...{[`data-featured-${feature}`]: ""}}>{id + " " + feature}</div>
+                    )
+                })
             }
         </div>
     )
